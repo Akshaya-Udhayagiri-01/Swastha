30df0949a8887bafd874f37d169aa4759d36b225
Akshaya Udhayagiri <udhayagiriakshaya@gmail.com>
2025-03-25 00:08:04 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -22,3 +22,6 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 app.use("/api/moods", require("./routes/moodRoutes"));
 app.use("/api/ai", require("./routes/aiRoutes"));
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to Swastha API" });
+});
